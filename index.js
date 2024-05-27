4e09f44c658831a62af35a8ce9772ab73d63c245
Juan Cardenas <juanancardenas@gmail.com>
2024-05-27 15:38:53 +0200

    First version of the proyecto 1-a

---
+++ index.js
@@ -0,0 +1,31 @@
+module.exports = {
+
+    /**
+     * @example
+     * n1 = 1, n2 = 4 -> Returns 5
+     * @param {*} n1 First number of the add
+     * @param {*} n2 Second number of the add
+     */
+    suma: function (n1, n2) {
+        return n1 + n2;
+    },
+
+    /**
+     * @example
+     * n1 = 1, n2 = 4 -> Returns -3
+     * @param {*} n1 First number of the substract
+     * @param {*} n2 Second number of the substract
+     */
+    resta: function (n1, n2) {
+        return n1 - n2;
+    },
+
+    multiplicacion: function (n1, n2) {
+        return n1 * n2;
+    },
+
+    division: function (n1, n2) {
+        return n1 / n2;
+    },
+
+}
---
+++ pruebas.js
@@ -0,0 +1,7 @@
+const oTest = require('.');
+
+console.log(oTest.suma(1,2));
+
+console.log(oTest.suma(1,2));
+
+console.log(oTest.resta(11,24));
